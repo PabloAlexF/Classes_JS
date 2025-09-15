f02fdf7817ec63dac292b5ea86ce4074548f4f3a
Pablo <Pablolexandrade@gmail.com>
2025-09-15 17:55:53 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// INPUTS:
+
+
+
+class User {
+    constructor(name,email,password,photo) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.photo = photo;
+    }
+
+
+}
